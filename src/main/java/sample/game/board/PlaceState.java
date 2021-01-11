1f002673c41a37a4a3643efbabeaae82876061f2
cifot <TMMurtuzov@sberbank.ru>
2021-01-11 03:26:02 +0300

    GOMOKU-1 изменил состояния ячеек на поле

--- src/main/java/sample/game/board/PlaceState.java
+++ src/main/java/sample/game/board/PlaceState.java
@@ -1,37 +1,39 @@
 package sample.game.board;
 
 public enum PlaceState {
-    EMPTY("    -fx-content-display: graphic-only;\n" +
-            "    -fx-graphic-text-gap: 0.0;\n" +
-            "    -fx-min-height: 10.0;\n" +
-            "    -fx-pref-height: 18.0;\n" +
-            "    -fx-min-width: 10.0;\n" +
-            "    -fx-pref-width: 18.0;\n" +
-            "    -fx-background-color: #c0c0c0;\n" +
-            "    -fx-background-radius: 50"),
-    WHITE("    -fx-content-display: graphic-only;\n" +
-            "    -fx-graphic-text-gap: 0.0;\n" +
-            "    -fx-min-height: 10.0;\n" +
-            "    -fx-pref-height: 32.0;\n" +
-            "    -fx-min-width: 10.0;\n" +
-            "    -fx-pref-width: 32.0;\n" +
-            "    -fx-background-color: #ffffff;\n" +
-            "    -fx-background-radius: 50"),
-    BLACK("    -fx-content-display: graphic-only;\n" +
-            "    -fx-graphic-text-gap: 0.0;\n" +
-            "    -fx-min-height: 10.0;\n" +
-            "    -fx-pref-height: 32.0;\n" +
-            "    -fx-min-width: 10.0;\n" +
-            "    -fx-pref-width: 32.0;\n" +
-            "    -fx-background-color: #000000;\n" +
-            "    -fx-background-radius: 50");
-
-    private String style;
-    PlaceState(String style) {
-        this.style = style;
-    }
+    EMPTY, WHITE, BLACK;
 
     public String getStyle() {
+        String style;
+        switch (this) {
+            case BLACK -> style = "    -fx-content-display: graphic-only;\n" +
+                    "    -fx-graphic-text-gap: 0.0;\n" +
+                    "    -fx-min-height: 10.0;\n" +
+                    "    -fx-pref-height: 32.0;\n" +
+                    "    -fx-min-width: 10.0;\n" +
+                    "    -fx-pref-width: 32.0;\n" +
+                    "    -fx-background-color: #000000;\n" +
+                    "    -fx-background-radius: 50";
+
+            case WHITE -> style = "    -fx-content-display: graphic-only;\n" +
+                    "    -fx-graphic-text-gap: 0.0;\n" +
+                    "    -fx-min-height: 10.0;\n" +
+                    "    -fx-pref-height: 32.0;\n" +
+                    "    -fx-min-width: 10.0;\n" +
+                    "    -fx-pref-width: 32.0;\n" +
+                    "    -fx-background-color: #ffffff;\n" +
+                    "    -fx-background-radius: 50";
+
+            case EMPTY -> style = "    -fx-content-display: graphic-only;\n" +
+                    "    -fx-graphic-text-gap: 0.0;\n" +
+                    "    -fx-min-height: 10.0;\n" +
+                    "    -fx-pref-height: 18.0;\n" +
+                    "    -fx-min-width: 10.0;\n" +
+                    "    -fx-pref-width: 18.0;\n" +
+                    "    -fx-background-color: #c0c0c0;\n" +
+                    "    -fx-background-radius: 50";
+            default -> throw new RuntimeException("Invalid style");
+        }
         return style;
     }
 }
